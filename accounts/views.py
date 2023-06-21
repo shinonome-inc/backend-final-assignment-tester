58ba364610a56f4ea457a033c5e5003494d0a297
masaya09 <74097011+masaya09@users.noreply.github.com>
2023-06-22 00:17:08 +0900

    フォーマット修正

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
-from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .forms import SignupForm
 
